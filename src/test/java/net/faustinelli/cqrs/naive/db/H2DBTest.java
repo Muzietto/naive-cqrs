82c7d1fa60ebc1642acbc79c322a491d589e2790
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-25 17:05:50 +0100

    Update H2DBTest.java

--- src/test/java/net/faustinelli/cqrs/naive/db/H2DBTest.java
+++ src/test/java/net/faustinelli/cqrs/naive/db/H2DBTest.java
@@ -1,9 +1,12 @@
 package net.faustinelli.cqrs.naive.db;
 
+import net.faustinelli.cqrs.naive.model.Conference;
+import net.faustinelli.cqrs.naive.model.Message;
 import org.hibernate.Transaction;
 import org.hibernate.classic.Session;
 import org.junit.Test;
 
+import static net.faustinelli.cqrs.naive.db.HibernateConfig.getFactory;
 import static net.faustinelli.cqrs.naive.db.HibernateConfig.getSession;
 import static org.junit.Assert.assertEquals;
 
@@ -48,7 +51,7 @@ public class H2DbTest {
     @Test
     public void secondDbTest() {
         Session session;
-        Message message;
+        Conference conference;
         Transaction tx;
 
         session = getSession();
@@ -56,24 +59,24 @@ public class H2DbTest {
 
         // PLAY HERE
 
-        message = new Message("Hello World");
-        session.save(message);
+        conference = new Conference("Hello World");
+        session.save(conference);
         tx.commit();
 
         session = getSession();
         tx = session.beginTransaction();
         // 1 is the generated id of the first message
-        message = (Message) session.load(Message.class, new Long(1));
-        message.setText("Greetings Earthling");
-        assertEquals("Greetings Earthling", message.getText());
-        Message nextMessage = new Message("Take me to your leader (please)");
-        message.setNextMessage(nextMessage);
+        conference = (Conference) session.load(Conference.class, new Long(1));
+        conference.setTitle("Great Confy");
+        assertEquals("Great Confy", conference.getTitle());
+        Integer numSeats = new Integer(9);
+        conference.setNumSeats(numSeats);
         tx.commit();
 
         session = getSession();
         tx = session.beginTransaction();
-        message = (Message) session.load(Message.class, new Long(2));
-        assertEquals("Take me to your leader (please)", message.getText());
+        conference = (Conference) session.load(Conference.class, new Long(1));
+        assertEquals(new Integer(9), conference.getNumSeats());
         tx.commit();
     }
 
