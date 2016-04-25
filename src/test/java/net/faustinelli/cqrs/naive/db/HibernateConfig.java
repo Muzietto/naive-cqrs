6d71de4630bdb7003de34dc6b695dd30324537a2
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-25 16:45:09 +0100

    Update HibernateConfig.java

--- src/test/java/net/faustinelli/cqrs/naive/db/HibernateConfig.java
+++ src/test/java/net/faustinelli/cqrs/naive/db/HibernateConfig.java
@@ -8,6 +8,7 @@
 
 package net.faustinelli.cqrs.naive.db;
 
+import net.faustinelli.cqrs.naive.model.Conference;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.annotations.Entity;
@@ -18,45 +19,19 @@ import org.hibernate.classic.Session;
  * Created by Marco Faustinelli (Muzietto) on 4/25/2016.
  */
 public class HibernateConfig {
+    private static SessionFactory THE_FACTORY = new Configuration()
+            .addAnnotatedClass(Entity.class)
+            .addAnnotatedClass(Message.class)
+            .addAnnotatedClass(Conference.class)
+            .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
+            .setProperty("hibernate.connection.url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
+            .setProperty("hibernate.current_session_context_class", "thread")
+            .setProperty("hibernate.show_sql", "true")
+            .setProperty("hibernate.hbm2ddl.auto", "create")
+            .setProperty("log4j.logger.org.hibernate", "warn")
+            .buildSessionFactory();
 
-    public static void main(String[] args) throws Exception {
-        SessionFactory sessionFactory = new Configuration()
-                .addAnnotatedClass(Entity.class)
-                .addAnnotatedClass(Message.class)
-                .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
-                .setProperty("hibernate.connection.url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
-                .setProperty("hibernate.current_session_context_class", "thread")
-                .setProperty("hibernate.show_sql", "true")
-                .setProperty("hibernate.hbm2ddl.auto", "create")
-                .setProperty("log4j.logger.org.hibernate", "warn")
-                .buildSessionFactory();
-
-        Session session;
-        Message message;
-        Transaction tx;
-
-        session = sessionFactory.getCurrentSession();
-        tx = session.beginTransaction();
-
-        // PLAY HERE
-
-        message = new Message("Hello World");
-        session.save(message);
-        tx.commit();
-
-        session = sessionFactory.getCurrentSession();
-        tx = session.beginTransaction();
-        // 1 is the generated id of the first message
-        message = (Message) session.load(Message.class, new Long(1));
-        System.out.println("---------------> " + message.getText() + " <------------------------");
-        message.setText("Greetings Earthling");
-        Message nextMessage = new Message("Take me to your leader (please)");
-        message.setNextMessage(nextMessage);
-        tx.commit();
-
-
-//        session.close();
-
-
+    public static Session getSession() {
+        return THE_FACTORY.getCurrentSession();
     }
 }
