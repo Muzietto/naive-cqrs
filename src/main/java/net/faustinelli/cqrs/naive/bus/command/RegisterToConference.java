1234f1073d743a156b88b5303f7ec773f0d237df
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-26 15:43:36 +0100

    Create RegisterToConference.java

---
+++ src/main/java/net/faustinelli/cqrs/naive/bus/command/RegisterToConference.java
@@ -0,0 +1,24 @@
+/*
+ * Project: naive-cqrs
+ * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
+ * Web: http://faustinelli.wordpress.com/, https://www.github.com/muzietto, http://faustinelli.net/
+ * Version: 1.0
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive-cqrs Project
+ */
+
+package net.faustinelli.cqrs.naive.bus.command;
+
+import net.faustinelli.cqrs.naive.model.Conference;
+
+/**
+ * Created by Marco Faustinelli (Muzietto) on 4/26/2016.
+ */
+public class RegisterToConference implements Command {
+    private Conference conference;
+
+    public RegisterToConference(Conference conference) {
+        this.conference = conference;
+    }
+
+
+}
