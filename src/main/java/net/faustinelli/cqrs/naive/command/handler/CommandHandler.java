9edcde3fd69c23814d5ec41a7f68d853947cbda7
Muzietto <marco_faustinelli@yahoo.com>
2016-04-26 07:16:55 +0100

    command handlers

---
+++ src/main/java/net/faustinelli/cqrs/naive/command/handler/CommandHandler.java
@@ -0,0 +1,19 @@
+/*
+ * Project: naive_cqrs
+ * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
+ * Web: http://faustinelli.wordpress.com/, http://www.github.com/muzietto, http://faustinelli.net/
+ * Version: 1.0
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive_cqrs Project
+ */
+
+package net.faustinelli.cqrs.naive.command.handler;
+
+import net.faustinelli.cqrs.naive.command.Command;
+
+/**
+ * Created by Marco Faustinelli (Muzietto) on 26/04/2016.
+ */
+public abstract class CommandHandler {
+
+    public abstract void handle(Command command);
+}
