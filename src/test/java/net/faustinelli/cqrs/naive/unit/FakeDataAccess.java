fccb2a65edac792e1c3174809678ec7acfff3a4d
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-26 17:21:36 +0100

    Create FakeDataAccess.java

---
+++ src/test/java/net/faustinelli/cqrs/naive/unit/FakeDataAccess.java
@@ -0,0 +1,18 @@
+/*
+ * Project: naive-cqrs
+ * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
+ * Web: http://faustinelli.wordpress.com/, https://www.github.com/muzietto, http://faustinelli.net/
+ * Version: 1.0
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive-cqrs Project
+ */
+
+package net.faustinelli.cqrs.naive.unit;
+
+import net.faustinelli.cqrs.naive.db.DataAccess;
+import org.hibernate.classic.Session;
+
+/**
+ * Created by Marco Faustinelli (Muzietto) on 4/26/2016.
+ */
+public class FakeDataAccess implements DataAccess {
+}
