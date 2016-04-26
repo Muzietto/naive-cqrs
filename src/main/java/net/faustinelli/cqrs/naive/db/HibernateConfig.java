ebfa7ee668ad36a5a69c1ee1e3fb8a0208f732e7
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-26 17:16:56 +0100

    Update HibernateConfig.java

--- src/main/java/net/faustinelli/cqrs/naive/db/HibernateConfig.java
+++ src/main/java/net/faustinelli/cqrs/naive/db/HibernateConfig.java
@@ -1,7 +1,7 @@
 /*
  * Project: naive-cqrs
  * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
- * Web: http://faustinelli.wordpress.com/, http://www.github.com/muzietto, http://faustinelli.net/
+ * Web: http://faustinelli.wordpress.com/, https://www.github.com/muzietto, http://faustinelli.net/
  * Version: 1.0
  * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive-cqrs Project
  */
@@ -18,25 +18,33 @@ import org.hibernate.classic.Session;
 /**
  * Created by Marco Faustinelli (Muzietto) on 4/25/2016.
  */
-public class HibernateConfig {
-    private static SessionFactory THE_FACTORY = new Configuration()
-            .addAnnotatedClass(Conference.class)
-            .addAnnotatedClass(SeatAvailability.class)
-            .addAnnotatedClass(Message.class)
-            .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
-            .setProperty("hibernate.connection.url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
-            .setProperty("hibernate.current_session_context_class", "thread")
-            .setProperty("hibernate.show_sql", "true")
-            .setProperty("hibernate.hbm2ddl.auto", "create")
-            .setProperty("javax.persistence.validation.mode", "none")
-            .setProperty("log4j.logger.org.hibernate", "warn")
-            .buildSessionFactory();
+public class HibernateConfig implements DataAccess {
+    private static HibernateConfig INSTANCE;
+    private static SessionFactory THE_FACTORY = null;
 
-    public static SessionFactory getFactory() {
-        return THE_FACTORY;
+    private HibernateConfig() {
+        THE_FACTORY = new Configuration()
+                .addAnnotatedClass(Conference.class)
+                .addAnnotatedClass(SeatAvailability.class)
+                .addAnnotatedClass(Message.class)
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
+                .setProperty("hibernate.connection.url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
+                .setProperty("hibernate.current_session_context_class", "thread")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.hbm2ddl.auto", "create")
+                .setProperty("javax.persistence.validation.mode", "none")
+                .setProperty("log4j.logger.org.hibernate", "warn")
+                .buildSessionFactory();
     }
 
     public static Session getSession() {
+        if (THE_FACTORY == null) {
+            INSTANCE = new HibernateConfig();
+        }
         return THE_FACTORY.getCurrentSession();
+    }
+
+    public static void setFactory(SessionFactory aFactory) {
+        THE_FACTORY = aFactory;
     }
 }
