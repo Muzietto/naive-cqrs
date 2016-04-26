c7a4683e5886822eb8dcefdeb8408ff75e7eaee0
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-26 14:29:58 +0100

    Update OrderCommandHandler.java

--- src/main/java/net/faustinelli/cqrs/naive/command/handler/OrderCommandHandler.java
+++ src/main/java/net/faustinelli/cqrs/naive/command/handler/OrderCommandHandler.java
@@ -1,25 +1,23 @@
 /*
- * Project: naive_cqrs
+ * Project: naive-cqrs
  * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
- * Web: http://faustinelli.wordpress.com/, http://www.github.com/muzietto, http://faustinelli.net/
+ * Web: http://faustinelli.wordpress.com/, https://www.github.com/muzietto, http://faustinelli.net/
  * Version: 1.0
- * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive_cqrs Project
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive-cqrs Project
  */
 
-package net.faustinelli.cqrs.naive.command.handler;
+package net.faustinelli.cqrs.naive.bus.command.handler;
 
-import net.faustinelli.cqrs.naive.command.Command;
-import net.faustinelli.cqrs.naive.command.RegisterToConference;
+import com.google.gwt.thirdparty.guava.common.eventbus.Subscribe;
+import net.faustinelli.cqrs.naive.bus.command.RegisterToConference;
 
 /**
- * Created by Marco Faustinelli (Muzietto) on 26/04/2016.
+ * Created by Marco Faustinelli (Muzietto) on 4/26/2016.
  */
-public class OrderCommandHandler extends CommandHandler {
+public class OrderCommandHandler implements CommandHandler {
 
-    public void handle(RegisterToConference command) {}
-
-    @Override
-    public void handle(Command command) {
-        
+    @Subscribe
+    public void handle(RegisterToConference command) {
+        System.out.println("qweqweqweqweqweqweqwe");
     }
 }
