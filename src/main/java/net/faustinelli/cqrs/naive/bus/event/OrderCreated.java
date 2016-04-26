14ccdfaf8d0d74dd9bd7ec85e426f5de6a684513
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-26 15:40:32 +0100

    Create OrderCreated.java

---
+++ src/main/java/net/faustinelli/cqrs/naive/bus/event/OrderCreated.java
@@ -0,0 +1,15 @@
+/*
+ * Project: naive-cqrs
+ * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
+ * Web: http://faustinelli.wordpress.com/, https://www.github.com/muzietto, http://faustinelli.net/
+ * Version: 1.0
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive-cqrs Project
+ */
+
+package net.faustinelli.cqrs.naive.bus.event;
+
+/**
+ * Created by Marco Faustinelli (Muzietto) on 4/26/2016.
+ */
+public class OrderCreated implements Event {
+}
