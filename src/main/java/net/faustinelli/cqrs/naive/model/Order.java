145c18ae486608141e57a236d8781625c37a2359
Marco Faustinelli <marco_faustinelli@yahoo.com>
2016-04-26 17:19:40 +0100

    Create Order.java

---
+++ src/main/java/net/faustinelli/cqrs/naive/model/Order.java
@@ -0,0 +1,23 @@
+/*
+ * Project: naive-cqrs
+ * Author: Marco Faustinelli - Muzietto (contacts@faustinelli.net)
+ * Web: http://faustinelli.wordpress.com/, https://www.github.com/muzietto, http://faustinelli.net/
+ * Version: 1.0
+ * The GPL 3.0 License - Copyright (c) 2015-2016 - The naive-cqrs Project
+ */
+
+package net.faustinelli.cqrs.naive.model;
+
+/**
+ * Created by Marco Faustinelli (Muzietto) on 4/26/2016.
+ */
+public class Order {
+
+    private final Conference conference;
+    private final Integer seatsToBook;
+
+    public Order(Conference conf, Integer seatsToBook) {
+        this.conference = conf;
+        this.seatsToBook = seatsToBook;
+    }
+}
